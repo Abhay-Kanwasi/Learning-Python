d4ce6b9d895741664c6115795b8de52b1ef7d6e6
Abhay Kanwasi <abhaykanwasi@itgopeshwar.ac.in>
2024-08-11 19:33:47 +0000

    Question added

--- Revision/List/Questions.py
+++ Revision/List/Questions.py
@@ -1,12 +1,17 @@
+from copy import deepcopy
+from copy import copy
+
 # 1. Write a Python program to sum all the items in a list. 
-# print(sum([1,2,3,5]))
+print(sum([1,2,3,5]))
+
 
 # 2. Write a Python program to multiply all the items in a list. 
-# numbers = [2,4,5,4]
-# multiply = 1
-# for i in numbers:
-#     multiply*=i
-# print(multiply)
+numbers = [2,4,5,4]
+multiply = 1
+for i in numbers:
+    multiply*=i
+print(multiply)
+
 
 # 3. Write a python program to get largest number in the list
 
@@ -21,19 +26,21 @@ for number in number_list:
     if number > max_number:
         max_number = number
 
-print(f'max number approch 2 : {max_number}')
+# print(f'max number approch 2 : {max_number}')
+
 
 # 4. Write a Python program to get the smallest number from a list. 
 
 # Approch 1
-print(f'min number approch 1 : {min(number_list)}')
+# print(f'min number approch 1 : {min(number_list)}')
 
 # Approch 2
 min_number = number_list[0]
 for number in number_list:
     if number < min_number:
         min_number = number
-print(f'min number approch 2 : {min_number}')
+# print(f'min number approch 2 : {min_number}')
+
 
 # 5. Write a Python program to count the number of strings from a given list of strings. The string length is 2 or more and the first and last characters are the same.
 
@@ -44,4 +51,116 @@ for string in sample_list:
     if len(string) >= 2 and string[0] == string[-1]:
         output_strings.append(string)
 
-print(f'outupt string {output_strings}')
+# print(f'outupt string {output_strings}')
+
+
+# 6. Write a Python program to get a list, sorted in increasing order by the last element in each tuple from a given list of non-empty tuples.
+
+sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+expected_result = [(2, 1), (1, 2), (2, 3), (4, 4), (2, 5)]
+
+def last(n):
+    return n[-1]
+
+sample = [12,3,14,5]
+# print(sorted(sample_list, key=last)) 
+
+
+# 7. Write a Python program to remove duplicates from a list.
+
+# Approch 1
+list1 = [9,8,3,2,1,1,8]
+print(f'list without duplicates approch1', list(set(list1)))
+
+# Approch 2
+list_without_duplicates = []
+for number in list1:
+    if number in list_without_duplicates:
+        continue
+    else:
+        list_without_duplicates.append(number)
+print(f'list without duplicates approch2{list_without_duplicates}')
+
+# Approch 3
+import pandas as pd
+df = pd.DataFrame(list1)
+list_without_duplicates = df.drop_duplicates()
+print(f"list without duplicates approch3 {list_without_duplicates.values.flatten().tolist()}")
+
+
+# 8. Write a Python program to check if a list is empty or not.
+
+def check_list_is_empty_or_not_1(list):
+    if len(list) == 0:
+        return 'List is empty'
+    else:
+        return 'List is not empty'
+    
+def check_list_is_empty_or_not_2(list):
+    try:
+        list[0]
+        return 'List is not empty'
+    except IndexError:
+        return 'List is empty'
+    
+
+# 9. Write a Python program to clone or copy a list.
+
+def clone_a_list_shallow_copy(list):
+    copy_list = list.copy()
+    return copy_list
+
+def copy_a_list_deep_copy(list):
+    copy_list = deepcopy(list)
+    return copy_list
+
+def copy_list(list):
+    copy_list = []
+    for element in list:
+        copy_list.append(element)
+    return copy_list
+
+def copy_list_shallow_copy(list):
+    copy_list = copy(list)
+    return copy_list
+
+
+# 10. Write a Python program to find the list of words that are longer than n from a given list of words.
+
+def words_that_are_longer_then(this_number):
+    longer_then_this_number_list = []
+    list_of_words = ['dinesh', 'abhi', 'iljam', 'jam', 'oi', 'a', 'senioritta']
+    for word in list_of_words:
+        if len(word) > this_number:
+            longer_then_this_number_list.append(word)
+    return longer_then_this_number_list
+
+
+# 11. Write a Python function that takes two lists and returns True if they have at least one common member.
+
+def common_member_in_two_list(list1, list2):
+    status = False
+    for element in list1:
+        for this_element in list2:
+            if element == this_element:
+                status = True
+    if status:
+        return True
+    else:
+        return False
+
+# 12 . Write a Python program to print a specified list after removing the 0th, 4th and 5th elements.
+sample_list = ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+# Expected Output : ['Green', 'White', 'Black']
+
+def specified_list_after_removing_0th_4th_5th_elements(list):
+    specified_list = []
+    for element in range(len(list)):
+        if element != 0 and element != 4 and element != 5:
+            specified_list.append(list[element])
+    return specified_list
+
+
+print(specified_list_after_removing_0th_4th_5th_elements(sample_list))
+
+        
