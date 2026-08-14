977742c6997b97f866aafea7bb47c4925d119f1f
Abhay Kanwasi <abhaykanwasi123@gmail.com>
2026-08-14 22:14:02 +0530

    Tuple started

---
+++ Let_Us_Python/Tuples/practice.py
@@ -0,0 +1,21 @@
+# Tuples
+
+# Tuples are typically used for handling heterogenous data.
+
+# Declare Tuple
+tup = ()
+print(type(tup))
+
+tup = (10, )
+print(type(tup))
+
+tup = tuple([2, 4])
+
+tup = 1, 2, 4
+
+
+# Tuples are immutable
+
+# Access tuple elements
+msg = ("Handle", "Exception", "Like", "Ravi")
+print(msg[1])
