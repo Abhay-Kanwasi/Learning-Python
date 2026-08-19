6204d3bf31f75cffc48c032a2cab42cb62d0d3a7
Abhay Kanwasi <abhaykanwasi123@gmail.com>
2026-08-19 22:51:31 +0530

    Add practice examples for sets and enhance tuple exercises with new questions

---
+++ Let_Us_Python/Set/practice.py
@@ -0,0 +1,66 @@
+# SET
+"""
+Set are like lists, with an exception that they do not contain duplicate elements/entries.
+
+variable = set() 
+variable = {<value>} # {20}
+variable = {<str>, <int>}  # {'Abhay', 5}
+
+Set is an unordered collection. Hence order of inserting is not same as the order of access. 
+
+set() function can be used to convert a string, list, or tuple into the set.
+
+Sets are mutable there contents can be changed
+
+If we want a immutable set, we use frozenset
+
+A set cannot contain a set embedded in it.
+
+Two sets cannot be added to create a third set.
+
+# Accessing Set Elements
+- Unordered collection, items in set cannot be accessed using incdices.
+- Set cannot be sliced using []
+- Entire set can be printed by just using the name of the set. 
+- Iterated over using a loop
+
+# Operations
+- Built-in functions and common set operations are shown below
+s = {1, 2, 4, 5, 3}
+in = 12 in s
+not in = 12 not in s
+len = len(s)
+max = max(s)
+min = min(s)
+sorted = sorted(s)
+sum = sum(s)
+
+# Methods
+s = {1, 2, 4, 5, 3}
+t = {'A', 'B', 'C'}
+
+s.update(t) # adds elements of t to s
+s.add('He') # adds 'He' to s
+s.remove(<value>) # delete value from s
+s.discard(<value>) # remove <value> would raised error discard won't 
+s.clear() # remove all elements
+
+# Set to check relationships between them.
+s = {1, 2, 4, 5, 3}
+t = {1, 3}
+
+s.isuperset(t) # True
+s.isubset(t) # False
+s.isdisjoint(t) # False
+
+"""
+
+lst = [1, 3, 5, 6]
+tpl = ('Sanjana', 23, 2.2)
+s = 'Ocean'
+s1 = set(lst)
+s2 = set(tpl)
+s3 = set(s)
+print(f"s1={s1}\ns2={s2}\ns3={s3}\n")
+
+
--- Let_Us_Python/Tuples/practice.py
+++ Let_Us_Python/Tuples/practice.py
@@ -3,19 +3,68 @@
 # Tuples are typically used for handling heterogenous data.
 
 # Declare Tuple
-tup = ()
-print(type(tup))
+# tup = ()
+# print(type(tup))
 
-tup = (10, )
-print(type(tup))
+# tup = (10, )
+# print(type(tup))
 
-tup = tuple([2, 4])
+# tup = tuple([2, 4])
 
-tup = 1, 2, 4
+# tup = 1, 2, 4
 
 
-# Tuples are immutable
+# # Tuples are immutable
 
-# Access tuple elements
-msg = ("Handle", "Exception", "Like", "Ravi")
-print(msg[1])
+# # Access tuple elements
+# msg = ("Handle", "Exception", "Like", "Ravi")
+# print(msg[1])
+
+
+"""QUESTIONS"""
+
+# Pass a tuple to the divmod() function and obtain the quotaient and the remainder.
+
+# result = divmod(17, 3)
+# print(result)
+
+# tup = (17, 3)
+# result = divmod(*tup)
+# print(result)
+
+
+"""
+Create a program to perform some actions:
+- Pack first 10 multiples of 10 into a tuple
+- Unpack the tuple into 10 variables, each holding 1 value
+- Unpack the tuple such that first value gets stored in variable x, last value in y and all values in between into disposable variables _
+- Unpack the tuple such that first value gets stored in variable i, last value in j and all values in between into a single disposable variables _
+"""
+
+# tpl = (10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
+# a, b, c, d, e, f, g, h, i, j = tpl
+# # print(tpl)
+# x, _, _, _, _, _, _, _, _, y = tpl
+# print(x, y, _)
+
+
+# i, *_, j = tpl
+# print(i, j, _)
+
+"""
+A list contains names of boys and girls as it's elements. Boys names are stored in tuples. Write a program to find out numer of boys and girls in the list.
+"""
+lst = [('Abhay',), ('Ravi',), 'Nisha', 'Simran', 'Radha']
+
+boys = 0
+girls = 0
+
+for element in lst:
+    if type(element) == tuple:
+        boys+=1
+    else:
+        girls+=1
+
+print(f'Boys {boys}\nGirls {girls}')
+
+
