4dc143608b17f5c04e99d652219f77bbf58595e0
Abhay Kanwasi <abhaykanwasi123@gmail.com>
2026-08-21 22:10:21 +0530

    Add initial dictionary documentation and examples

---
+++ Dictionary/practice1.py
@@ -0,0 +1,13 @@
+"""
+Dictionary
+
+It is a collection of key value pairs. Unlike sequence types they are indexed by keys.
+Also know as maps or associative arrays
+
+Keys in a dictionary must be unique and immutable. So strings or tuples can be used as keys as they are immutable.
+
+Ways to create dictionary
+
+a = {} # a = {key : value, key : value}
+b = {"A1" : "Abhay", "R1" : "Ravi"}
+"""
